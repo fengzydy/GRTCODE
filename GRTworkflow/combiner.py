cf77a882f64dea5482f68fdca730526e233d1d40
Jing Feng <Jing.Feng@gaea53.ncrc.gov>
2025-07-14 14:28:05 -0400

    add workflow scripts

---
+++ GRTworkflow/combiner.py
@@ -0,0 +1,47 @@
+import os
+import glob
+import xarray as xr
+import argparse
+
+def combine_netcdf_files(input_files, output_file):
+    """
+    Combine NetCDF files by concatenating along the 'lon' dimension.
+    Only variables starting with 'rl' are included, and longitude is globally sorted.
+    """
+    datasets = []
+    for file in input_files:
+        ds = xr.open_dataset(file)
+
+        # Filter variables starting with 'rl'
+        rl_vars = {var: ds[var] for var in ds.data_vars if var.startswith('rl')}
+        rl_ds = xr.Dataset(rl_vars, coords=ds.coords)
+
+        datasets.append(rl_ds)
+
+    # Concatenate and globally sort by 'lon'
+    combined_ds = xr.concat(
+        datasets,
+        dim='lon',
+        data_vars='all',
+        coords='all',
+        compat='override',
+        combine_attrs='drop'
+    ).sortby('lon')
+
+    # Save to NetCDF
+    combined_ds.to_netcdf(output_file)
+    print("Final longitude ordering:", combined_ds['lon'].values)
+    print(f"Combined NetCDF file saved to {output_file}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--workdir', type=str, required=True, help='Path to the working directory')
+    parser.add_argument('--year', type=int, required=True, help='Year of the data to combine')
+    parser.add_argument('--nametag', type=str, required=True, help='experiment name')
+    args = parser.parse_args()
+
+    file_pattern = f"{args.workdir}/{args.nametag}/{args.year}.era5-fluxes.nc.*"
+    output_file = f"{args.workdir}/{args.year}.{args.nametag}-cleansky-spectra.nc"
+
+    input_files = sorted(glob.glob(file_pattern))
+    combine_netcdf_files(input_files, output_file)
---
+++ GRTworkflow/environment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+source $MODULESHOME/init/bash
+
+module unload cray-netcdf cray-hdf5 fre
+module unload PrgEnv-pgi PrgEnv-intel PrgEnv-gnu PrgEnv-cray
+module load PrgEnv-intel/8.5.0
+module unload intel intel-classic intel-oneapi
+module load intel-classic/2023.2.0
+module load cray-hdf5/1.12.2.11
+module load cray-netcdf/4.9.0.9
+
+export KMP_STACKSIZE="512m"
+export NC_BLKSZ="1M"
+export F_UFMTENDIAN="big"
+
+PREFIX="/gpfs/f5/gfdl_m/scratch/Raymond.Menzel/line-by-line"
+CC="cc"
+CPPFLAGS="-I${PREFIX}/include"
+CFLAGS="-g -O3 -qopenmp"
+MPICC="cc"
+CXX="CC"
+CXXFLAGS="-g -O3 -qopenmp"
+MPICXX="CC"
+LDFLAGS="-L${PREFIX}/lib"
+
+#debug
+#CPPFLAGS="-I${PREFIX}/include"
+#CFLAGS="-g -O0 -fno-omit-frame-pointer -qopenmp"
+#CXXFLAGS="-g -O0 -fno-omit-frame-pointer -qopenmp"
